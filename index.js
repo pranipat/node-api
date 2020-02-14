6fefb5bdeda22cc4f309fa4ebcec8ec41fc78d9e
PRANIPAT KHATUA <pranipat@studymode.com>
2020-02-14 12:31:09 +0530

    Dockerisation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 require('dotenv').config();
 
+const promiseRetry = require('promise-retry');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
@@ -12,10 +14,20 @@ const dbName = process.env.MONGO_INITDB_DATABASE;
 const dbPort = process.env.MONGO_PORT;
 
 const url = `mongodb://${dbHOST}:${dbPort}/${dbName}`;
+
 const options = {
     useNewUrlParser: true,
     reconnectTries: 60,
-    reconnectInterval: 1000
+    reconnectInterval: 1000,
+    poolSize: 10,
+    bufferMaxEntries: 0 // If not connected, return errors immediately rather than waiting for reconnect
+}
+
+const promiseRetryOptions = {
+    retries: options.reconnectTries,
+    factor: 2,
+    minTimeout: options.reconnectInterval,
+    maxTimeout: 5000
 }
 
 const routes = require('./routes/routes.js');
@@ -30,18 +42,41 @@ app.use((req, res) => {
     res.status(404);
 });
 
-MongoClient.connect(url, options, (err, database) => {
-    if (err) {
-        console.log(`FATAL MONGODB CONNECTION ERROR: ${err}:${err.stack}`);
-        process.exit(1);
-    }
-    app.locals.db = database.db(dbName);
+const connect = () => {
+    return promiseRetry(async (retry, number) => {
+        console.log(`MongoClient connecting to ${url} - retry number: ${number}`)
+        try {
+            return MongoClient.connect(url, options, (err, database) => {
+                if(database){
+                    app.locals.db = database.db(dbName);
+                }                
+            });
+        }
+        catch (error) {
+            return retry(error);
+        }
+    }, promiseRetryOptions)
+}
+
+connect().then(()=>{
     http.listen(port, () => {
         console.log('Server started at: ' + `http://${HOST}:${port}`);
-        //console.log("Listening on port " + port);
-        //console.log(app.locals.db);
         app.emit('APP_STARTED');
     })
-})
+});
+
+// MongoClient.connect(url, options, (err, database) => {
+//     if (err) {
+//         console.log(`FATAL MONGODB CONNECTION ERROR: ${err}:${err.stack}`);
+//         process.exit(1);
+//     }
+//     app.locals.db = database.db(dbName);
+//     http.listen(port, () => {
+//         console.log('Server started at: ' + `http://${HOST}:${port}`);
+//         //console.log("Listening on port " + port);
+//         //console.log(app.locals.db);
+//         app.emit('APP_STARTED');
+//     })
+// })
 
 module.exports = app;
